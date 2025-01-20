63a6ceea520bdb4d9815da8893a0f04a5c0f1781
red5191 <the_red51@outlook.com>
2025-01-21 00:50:36 +0300

    fixed check

--- selenium_python_chrome.py
+++ selenium_python_chrome.py
@@ -23,13 +23,25 @@ base_url = 'http://demoqa.com/buttons'
 driver.get(base_url)
 driver.maximize_window()
 
-# создаем переменную для кнопки под двойной клик, нажимаем её и проверяем активацию
+# создаем переменную для кнопки под двойной клик и нажимаем её
 action = ActionChains(driver)
 double_click_button = driver.find_element(By.XPATH, "//button[@id='doubleClickBtn']")
 action.double_click(double_click_button).perform()
 print('Произвели двойной клик')
 
-# создаем переменную для кнопки под правый клик, нажимаем её и проверяем активацию
+# проверяем активацию кнопки под двойной клик
+message_double_click = driver.find_element(By.XPATH, "//p[@id='doubleClickMessage']").text
+print(f'Сообщение на сайте гласит: {message_double_click}')
+assert message_double_click == "You have done a double click", "Doubleclick-button not active"
+print("Doubleclick-button is active")
+
+# создаем переменную для кнопки под правый клик и нажимаем
 right_click_button = driver.find_element(By.XPATH, "//button[@id='rightClickBtn']")
 action.context_click(right_click_button).perform()
 print('Произвели правый клик')
+
+# проверяем активацию кнопки под двойной клик
+message_right_click = driver.find_element(By.XPATH, "//p[@id='rightClickMessage']").text
+print(f'Сообщение на сайте гласит: {message_right_click}')
+assert message_right_click == "You have done a right click", "Rightclick-button not active"
+print("Rightclick-button is active")
